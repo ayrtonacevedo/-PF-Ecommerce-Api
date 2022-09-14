9c29e0c7aa2479c67e2a1414da6dd82792813e0b
ayrtonacevedo <ayrtonacrc@gmail.com>
2022-09-14 14:33:39 -0300

    cambio carga de db

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const celularesRouter = require("./celulares");
+
 
 
 const router = Router();
@@ -10,7 +10,7 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
-router.use('/celulares', celularesRouter);
+
 
 
 module.exports = router;
