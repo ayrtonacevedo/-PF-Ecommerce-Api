aaf1eb056a8b011522ce988a1e3f674ffee6b7a4
12frankporx <f.s.b.rojas@gmail.com>
2022-09-14 21:07:20 -0500

    agregando filtrados

--- src/routes/celular.route.js
+++ src/routes/celular.route.js
@@ -1,17 +1,32 @@
 const {Router}=require('express')
-const {obtenerProductos,obtenerProductosById}=require('../Middleware/getProducto.middleware')
+const {obtenerProductos,obtenerProductosById, obtenerProductosByMarca, obtenerProductosByCapacidad, obtenerProductosByLinea, filtrarProductosMarcaAndLinea, obtenerProductosByStock, obtenerProductosByPrecio}=require('../Middleware/getProducto.middleware')
 const {crearProducto}=require('../Middleware/crearProducto.middleware')
 
 
 const router=Router();
 
 router.get('/', async(req,res,next)=>{
+    let {marca,linea,stockmin,stockmax,precio,capacidad}=req.query;
     try{
-        let productos = await obtenerProductos()
-        productos.length>0?
-        res.send(productos):res.send({message:"No products"})
+        if(marca && linea){
+            return res.status(200).json(await filtrarProductosMarcaAndLinea(marca,linea));
+        }else if(stockmin && stockmax){
+            return res.status(200).json(await obtenerProductosByStock(stockmin,stockmax));
+        }else if(precio){
+            return res.status(200).json(await obtenerProductosByPrecio(precio));
+        }else if(capacidad){
+            return res.status(200).json(await obtenerProductosByCapacidad(capacidad));
+        }else if(marca){
+            return res.status(200).json(await obtenerProductosByMarca(marca));
+        }else if(linea){
+            return res.status(200).json(await obtenerProductosByLinea(linea));
+        }else{
+            let productos = await obtenerProductos()
+            productos.length>0?
+            res.send(productos):res.send({message:"No products"})
+        }
     }
-    catch(error){next(error); console.log(error)}
+    catch(error){next(error.message); console.log(error.message)}
 })
 
 router.get('/:id', async(req,res,next)=>{
@@ -32,6 +47,7 @@ router.post('/',async(req,res,next)=>{
     }
     catch(error){next(error)}
 })
+
 
 
 
