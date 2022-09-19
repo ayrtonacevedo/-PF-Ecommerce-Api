4381280e99b1e7ef404c1d6f5554cdf7c9ea8223
12frankporx <f.s.b.rojas@gmail.com>
2022-09-18 22:32:25 -0500

    se ah agregado pasarela de pago

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,15 +3,16 @@ const { Router } = require('express');
 const brand=require('./marca.route')
 const cell=require('./celular.route')
 const user = require('./user.route')
+const checkout=require("./checkout.route")
 
 
 
 
 const router = Router();
-
 router.use('/celulares',cell)
 router.use('/marcas',brand)
 router.use('/users', user)
+router.use('/checkout', checkout)
 
 
 
