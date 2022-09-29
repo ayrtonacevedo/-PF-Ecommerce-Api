3a61713f4f3ff45e019b02d565571949aa8892e3
fvaldezz96 <valdezfede21@gmail.com>
2022-09-29 14:44:31 +0200

    addRating

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,21 @@
 const { Router } = require('express');
 
-const brand=require('./marca.route')
-const cell=require('./celular.route')
+const brand = require('./marca.route')
+const cell = require('./celular.route')
 const user = require('./user.route')
-const checkout=require("./checkout.route")
+const checkout = require("./checkout.route")
 const question = require('./question.route')
-const sendClaimMail=require('./sendClaimMail')
+const sendClaimMail = require('./sendClaimMail')
+const rating = require('./rating.router')
 
 const router = Router();
-router.use('/celulares',cell)
-router.use('/marcas',brand)
+router.use('/celulares', cell)
+router.use('/marcas', brand)
 router.use('/users', user)
 router.use('/checkout', checkout)
 router.use('/questions', question)
-router.use('/send-claim',sendClaimMail)
+router.use('/send-claim', sendClaimMail)
+router.use('/rating', rating)
 
 
 module.exports = router;
