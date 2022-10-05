b248190a3f30eb4da1dcb7e0d86fe533a8e56bbf
asdelaf <asanchezdelaf@hotmail.com>
2022-10-05 02:48:14 -0300

    float rating

--- src/models/Rating.js
+++ src/models/Rating.js
@@ -3,7 +3,7 @@ module.exports = (sequelize) => {
    // defino el modelo
    sequelize.define('rating',
       {
-         rating: { type: DataTypes.INTEGER, allowNull: false },
+         rating: { type: DataTypes.FLOAT, allowNull: false },
          emailUser: { type: DataTypes.STRING, allowNull: false },
          comment: { type: DataTypes.TEXT },
          date: { type: DataTypes.DATE, allowNull: false }
